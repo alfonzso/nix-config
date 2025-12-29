54348dc2fc199709684ef541a1c9e2888e83daab
Zsolt Alföldi <zsolt.alfoldi@nokia.com>
2025-12-29 04:05:10 +0100

    fixin clone_my_stuff and nvim headless install

--- nx/common/hm/systemdUser/clone_my_stuff.nix
+++ nx/common/hm/systemdUser/clone_my_stuff.nix
@@ -1,46 +1,75 @@
 { lib, pkgs, ... }: {
-  systemd.user.services.clone-my-stuff = {
-    Unit = {
-      Description = "Clone my github configuration if missing";
-      After = [ "default.target" ];
+  systemd.user.services = {
+
+    my-worker = {
+      Unit = {
+        Description = "Init nvim --headless";
+        After = [ "clone-my-stuff.service" ];
+      };
+
+      Service = {
+        Type = "simple";
+        Environment = with pkgs;
+          "PATH=${
+            lib.makeBinPath [
+              git
+              neovim
+              (rust-bin.selectLatestNightlyWith (toolchain:
+                toolchain.default.override {
+                  extensions = [ "rust-src" "rust-analyzer" ];
+                }))
+            ]
+          }:/run/current-system/sw/bin";
+        StandardOutput = "journal";
+        StandardError = "journal";
+        ExecStart = pkgs.writeShellScript "init-nvim" ''
+          set -ex
+          nvim --headless "+Lazy! restore" "+MasonToolsInstallSync" +qa
+        '';
+        Restart = "no";
+      };
+      Install = { WantedBy = [ "default.target" ]; };
     };
 
-    Service = {
-      Type = "oneshot";
-      Environment = "PATH=${
-          lib.makeBinPath [ pkgs.curl pkgs.git pkgs.openssh ]
-        }:/run/current-system/sw/bin";
-      ExecStart = pkgs.writeShellScript "clone-my-stuff" ''
-        set -ex
-        NVIM_DIR="$HOME/.config/nvim"
-        NIX_CFG_DIR="$HOME/workspace/home/nix/nix-config"
-        NIX_SEC_DIR="$HOME/workspace/home/nix/nix-secret"
+    clone-my-stuff = {
+      Unit = {
+        Description = "Clone my github configuration if missing";
+        After = [ "default.target" ];
+      };
 
-        if [ ! -d "$NVIM_DIR" ]; then
-          echo "Cloning Neovim config..."
-          git clone git@github.com:alfonzso/nvim.git $NVIM_DIR
-          nvim --headless "+Lazy! restore" "+MasonToolsInstallSync" +qa &
-          NVIM_PID=$!
-        fi
+      Service = {
+        Type = "oneshot";
+        Environment = with pkgs;
+          "PATH=${
+            lib.makeBinPath [ curl git openssh ]
+          }:/run/current-system/sw/bin";
+        ExecStart = pkgs.writeShellScript "clone-my-stuff" ''
+          set -ex
+          NVIM_DIR="$HOME/.config/nvim"
+          NIX_CFG_DIR="$HOME/workspace/home/nix/nix-config"
+          NIX_SEC_DIR="$HOME/workspace/home/nix/nix-secret"
 
-        if [ ! -d "$NIX_CFG_DIR" ]; then
-          echo "Cloning Nix config..."
-          mkdir -p "$NIX_CFG_DIR"
-          git clone git@github.com:alfonzso/nix-config $NIX_CFG_DIR
-        fi
+          if [ ! -d "$NVIM_DIR" ]; then
+            echo "Cloning Neovim config..."
+            git clone git@github.com:alfonzso/nvim.git $NVIM_DIR
+          fi
 
-        if [ ! -d "$NIX_SEC_DIR" ]; then
-          echo "Cloning Nix secrets..."
-          mkdir -p "$NIX_SEC_DIR"
-          git clone git@github.com:alfonzso/nix-secrets $NIX_SEC_DIR
-        fi
+          if [ ! -d "$NIX_CFG_DIR" ]; then
+            echo "Cloning Nix config..."
+            mkdir -p "$NIX_CFG_DIR"
+            git clone git@github.com:alfonzso/nix-config $NIX_CFG_DIR
+          fi
 
-        wait $NVIM_PID
-        echo "Nvim setup finished"
+          if [ ! -d "$NIX_SEC_DIR" ]; then
+            echo "Cloning Nix secrets..."
+            mkdir -p "$NIX_SEC_DIR"
+            git clone git@github.com:alfonzso/nix-secrets $NIX_SEC_DIR
+          fi
 
-      '';
+        '';
+      };
+
+      Install.WantedBy = [ "default.target" ];
     };
-
-    Install.WantedBy = [ "default.target" ];
   };
 }
