22f8c31f486986eada742e2c511dd85ae9b04961
alfonzso <alfonzso@gmail.com>
2026-01-06 16:05:47 +0100

    tweaking umask prop for users and shares over samba

--- nx/hosts/plgen8/default.nix
+++ nx/hosts/plgen8/default.nix
@@ -4,31 +4,6 @@ let
   _activations = _common + "/activations";
 in {
   imports = lib.flatten [
-    # ./hm
-    # "${_common}/hm"
-    #
-    # ./hardware-configuration.nix
-    # ./_global_host_config.nix
-    # ./modules/disko.nix
-    #
-    # # ./modules/config.nix
-    # # ./modules/mergerfs_4_samba.nix
-    # # ./modules/samba.nix
-    # # ./modules/nfs.nix
-    # # ./modules/mergerfs_4_nfs.nix
-    # # ./modules/mounts.nix
-    #
-    # "${_common}/_ssh.nix"
-    # "${_activations}/manage_ssh.nix"
-    # "${_common}/_sops/ssh.nix"
-    #
-    # ./modules/sops.nix
-    # "${_common}/_sops"
-    #
-    # "${_common}/_nix_conf.nix"
-    # "${_common}/_common_and_sys_env.nix"
-    # "${_common}/_networking.nix"
-    # "${_common}/_user.nix"
 
     ./hm
     "${_common}/hm"
@@ -36,60 +11,12 @@ in {
     ./modules/disko.nix
     ./modules/k8s.nix
     ./modules/sops.nix
+    ./modules/samba.nix
 
     ./hardware-configuration.nix
     ./_global_host_config.nix
 
     "${_activations}/manage_ssh.nix"
-
-    "${_common}/fileshare/user.nix"
-    "${_common}/fileshare/samba.nix"
-    {
-      services.samba.settings = lib.mkForce {
-        global = {
-          "workgroup" = "WORKGROUP";
-          "server string" = "smbnix";
-          "netbios name" = "smbnix";
-          "security" = "user";
-          "guest account" = "nobody";
-          "map to guest" = "bad user";
-        };
-        secure = {
-          path = "/mnt/secure";
-          browseable = "yes";
-          writable = "yes";
-          "read only" = "no";
-          "guest ok" = "no";
-          "valid users" = "${config.hostCfg.nasUser}";
-          "write list" = "${config.hostCfg.nasUser}";
-          "create mask" = "0755";
-          "directory mask" = "0755";
-        };
-        fast = {
-          path = "/mnt/fast";
-          browseable = "yes";
-          writable = "yes";
-          "read only" = "no";
-          "guest ok" = "no";
-          "valid users" = "${config.hostCfg.nasUser}";
-          "write list" = "${config.hostCfg.nasUser}";
-          "create mask" = "0755";
-          "directory mask" = "0755";
-        };
-      };
-
-      users.groups.zfs-storage = { gid = 990; };
-      users.users.${config.hostCfg.nasUser}.extraGroups = [ "zfs-storage" ];
-      systemd.tmpfiles.rules = [
-        "d /mnt/fast   2775 root zfs-storage - -"
-        "d /mnt/secure 2775 root zfs-storage - -"
-
-        # # More explicit ACLs (Default ACLs (Access Control Lists))
-        # # with this setup zfs-storage 775 permission will intherit to all files/folders
-        # "a+ /mnt/fast    - - - - default:user::rwx,default:group:zfs-storage:rwx,default:other::rx"
-        # "a+ /mnt/secure  - - - - default:user::rwx,default:group:zfs-storage:rwx,default:other::rx"
-      ];
-    }
 
     "${_common}/sops"
     "${_common}/sops/ssh.nix"
