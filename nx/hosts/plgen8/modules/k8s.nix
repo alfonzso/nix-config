d7d406be2a2b05b6af37aae6c1ccd8d38e96142c
alfonzso <alfonzso@gmail.com>
2026-01-10 17:21:27 +0100

    firewall 443 opened

--- nx/hosts/plgen8/modules/k8s.nix
+++ nx/hosts/plgen8/modules/k8s.nix
@@ -12,6 +12,7 @@ in {
 
   networking.firewall.allowedTCPPorts = [
     80 # enable for haproxy
+    443 # enable for haproxy
     6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
     # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
     # 2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
