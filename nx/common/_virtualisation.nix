e26e5e0f1f154ebbc0cd17fdc4a16f2c09214c39
alfonzso <alfonzso@gmail.com>
2026-07-06 23:29:07 +0200

    Enhance virtualisation configurations in Nix
    - Enabled libvirt daemon with QEMU support and added SWTPM and virtiofsd packages for improved container management.
    - Updated user group assignments to include 'kvm' and 'libvirtd' for enhanced permissions.
    - Expanded system packages to include SWTPM, virt-viewer, and virtio-win for better virtualisation capabilities.
    - Enabled Spice USB redirection for improved device handling in virtual environments.

--- nx/common/_virtualisation.nix
+++ nx/common/_virtualisation.nix
@@ -2,17 +2,33 @@
 {
   virtualisation = {
     containers.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        swtpm.enable = true;
+        vhostUserPackages = [ pkgs.virtiofsd ];
+      };
+    };
     podman = {
       enable = true;
       dockerCompat = true;
       defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
     };
+    spiceUSBRedirection.enable = true;
   };
+  programs.virt-manager.enable = true;
+
   users.users.${config.hostCfg.username} = {
-    extraGroups = [ "podman" ];
+    extraGroups = [ "kvm" "libvirtd" "podman" ];
   };
 
-  environment.systemPackages = with pkgs; [ qemu ];
+  environment.systemPackages = with pkgs; [
+    qemu
+    swtpm
+    virt-viewer
+    virtio-win
+  ];
 
   # # User namespace configuration
   # boot.kernel.sysctl = {
