c4b54726a6e57f37cb7b03b8e0bbcb32b757b6e4
alfonzso <alfonzso@gmail.com>
2026-07-23 18:37:27 +0200

    ssh secuhardening

--- nx/common/networking/ssh.nix
+++ nx/common/networking/ssh.nix
@@ -1,9 +1,21 @@
 { ... }: {
 
-  # Enable SSH
   services.openssh = {
     enable = true;
-    settings.X11Forwarding = true;
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "prohibit-password";
+      MaxAuthTries = 3;
+      LoginGraceTime = 30;
+      PerSourcePenalties = "authfail:3600s max:86400s";
+      X11Forwarding = true;
+    };
+  };
+
+  services.sshguard = {
+    enable = true;
+    services = [ "sshd" "sshd-session" ];
   };
 
 }
